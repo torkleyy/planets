0a1ec0cf99b1f65cb96e9fd86776cefe78f417a5
_Jolly_ <markus.budeus@web.de>
2017-10-17 16:04:54 +0200

    I guess I somehow fixed my project again. I removed unused imports.

--- app/src/main/java/mainburg/planetenweg/MapActivity.java
+++ app/src/main/java/mainburg/planetenweg/MapActivity.java
@@ -1,25 +1,14 @@
 package mainburg.planetenweg;
 
-import android.*;
-import android.Manifest;
-import android.content.Context;
 import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
-import android.support.v4.content.ContextCompat;
-import android.widget.Toast;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
 
