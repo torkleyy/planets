e5c2995472f042654c5fe11515cef4466884c41a
Thomas Schaller <torkleyy@gmail.com>
2017-11-13 19:11:57 +0100

    Make MainActivity the only main activity and add button logic (#5)

--- app/src/main/java/mainburg/planetenweg/MainActivity.java
+++ app/src/main/java/mainburg/planetenweg/MainActivity.java
@@ -1,13 +1,30 @@
 package mainburg.planetenweg;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button mapButton = (Button) findViewById(R.id.button_map);
+        mapButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_map:
+                startActivity(new Intent(this, MapActivity.class));
+                break;
+            default:
+                break;
+        }
     }
 }
